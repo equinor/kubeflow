e46e2fdb34d737b763db9042f32c1d921a7aef77
Subreptivus <Subreptivus@gmail.com>
2021-04-19 15:48:01 +0300

    Fix KNative probes

--- components/profile-controller/controllers/profile_controller.go
+++ components/profile-controller/controllers/profile_controller.go
@@ -381,6 +381,17 @@ func (r *ProfileReconciler) getAuthorizationPolicy(profileIns *profilev1.Profile
 					},
 				},
 			},
+			{
+				To: []*istioSecurity.Rule_To{
+					{
+						Operation: &istioSecurity.Operation{
+							// Workloads pathes should be accessible for KNative's
+							// `activator` and `controller` probes
+							Paths: []string{"/healthz", "/metrics"},
+						},
+					},
+				},
+			},
 		},
 	}
 }
